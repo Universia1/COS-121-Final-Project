1bb0f8aedcee09a8bb74a0ac2e6092fd1b53e964
Universia1 <flowerfellsansyt@gmail.com>
2024-12-19 01:14:42 -0500

    Final update to main()

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def main():
 	world["loc"] = "town_square"
 	
 	while True:
-		if player["enemy_slain"] == 100:
-			print(f"The skies open up and you hear a deep, entrancing voice slowly say:\n'Good job slaying 100 enemies, {player['name']}!' I, the Great Celestial Titan, bestow upon thee the Celestial Blade, the sword that created this realm during the great war of the titans.")
-			print("'Now, go on and defeat the fallen titan who slumbers in the ominous dungeon!'")
+		if player["enemy_slain"] == 100 and world["loc"] == "town_square":
+			print(f"The skies open up and you hear a deep, entrancing voice slowly say:\n'Good job slaying 100 enemies, {player['name']}!' I, the Great Celestial Titan, bestow upon thee the Celestial Blade, the sword that was thrust into the heart of the Great Destroyer, spilling the blood that created this realm during the great war of the titans.")
+			print("'Now, go on and defeat the Fallen Titan who slumbers in the ominous dungeon!'")
 			player["held_weapon"] = ["celestial blade"]
 		if player["HP"] <= 0:
 			print(f"{player['name']} has been defeated.")
@@ -22,7 +22,14 @@ def main():
 			print("Game Over.")
 			print("Thanks for playing!")
 			break
-			
+		elif fallen_titan["hp"] <= 0:
+			print("You defeated the Fallen Titan!")
+			print(f"You gained 1000G for defeating the Fallen Titan!")
+			player["current_G"] += 1000
+			gameWinSave(world, player)
+			print("You Win!\nThanks for playing!")
+			break
+		
 		if world["loc"] == "town_square":
 			showTown(world, player)
 		elif world["loc"] == "mysterious_cavern":
